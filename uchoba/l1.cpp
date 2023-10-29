dba2c09a82267c7228e9b6e2f88e52f872b96f47
SOVETSKAYA-CHERDACHNAYA-TABURETKA <rufusalastor@gmail.com>
2023-10-29 11:59:10 +0300

    homework 1

--- uchoba/l1.cpp
+++ uchoba/l1.cpp
@@ -2,6 +2,15 @@
 #include <string>
 using namespace std;
 void main(){
-
-	cout << "hello world";
+	int a;
+	int b;
+	int f;
+	cout << "insert number under letter a " << endl;
+	cin >> a;
+	cout << "insert number under letter b " << endl;
+	cin >> b;
+	cout << "insert number under letter f " << endl;
+	cin >> f;
+	cout << "the following formul will be used: (a+b-f/a)+f*a*a-(a+b) " << endl;
+	cout << (a + b - f / a) + f * a * a - (a + b) << endl;
 };
